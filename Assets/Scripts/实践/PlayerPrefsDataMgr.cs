9befe5bcf4a543cc55636525aa96230a54c642c2
GeonDBD <51849827+GeonDBD@users.noreply.github.com>
2023-02-22 11:32:45 +0800

    Update
    更新：
    - List、Dictionary以及自定义类型数据的存储与读取

--- Assets/Scripts/实践/PlayerPrefsDataMgr.cs
+++ Assets/Scripts/实践/PlayerPrefsDataMgr.cs
@@ -4,19 +4,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
 /// PlayerPrefs数据管理类 统一管理数据的存储和读取
 /// </summary>
 public class PlayerPrefsDataMgr
 {
+    // 单例实现
     private static PlayerPrefsDataMgr instance = new PlayerPrefsDataMgr();
-
     private PlayerPrefsDataMgr()
     {
 
     }
-
     public static PlayerPrefsDataMgr Instance
     {
         get { return instance; }
@@ -37,12 +37,10 @@ public class PlayerPrefsDataMgr
         for (int i = 0; i < infos.Length; i++)
         {
             info = infos[i];
-            // 定义key规则
-            // keyName_数据类型_字段类型_字段名
+            // 定义key规则 keyName_数据类型_字段类型_字段名
             saveKeyName = keyName + "_" + dataType.Name + "_" + info.FieldType.Name + "_" + info.Name;
-            //Debug.Log(saveKeyName);
 
-            // 封装存储值的方法
+            // 调用已封装存储值的方法
             SaveValue(info.GetValue(data), saveKeyName);
         }
         PlayerPrefs.Save();
@@ -56,6 +54,8 @@ public class PlayerPrefsDataMgr
     private void SaveValue(object value, string keyName)
     {
         Type valueType = value.GetType();
+
+        // 常用类型
         if (valueType == typeof(int))
         {
             PlayerPrefs.SetInt(keyName, (int)value);
@@ -72,6 +72,36 @@ public class PlayerPrefsDataMgr
         {
             // 自己定义的bool存储规则
             PlayerPrefs.SetInt(keyName, (bool)value ? 1 : 0);
+        }
+        // List
+        else if (typeof(IList).IsAssignableFrom(valueType))
+        {
+            IList list = value as IList;
+            PlayerPrefs.SetInt(keyName, list.Count);
+            int index = 0;
+            foreach (object item in list)
+            {
+                SaveValue(item, keyName + index);
+                index++;
+            }
+        }
+        // Dictionary
+        else if (typeof(IDictionary).IsAssignableFrom(valueType))
+        {
+            IDictionary dictionary = value as IDictionary;
+            PlayerPrefs.SetInt(keyName, dictionary.Count);
+            int index = 0;
+            foreach (object key in dictionary.Keys)
+            {
+                SaveValue(key, keyName + "_Key_" + index);
+                SaveValue(dictionary[key], keyName + "_Value_" + index);
+                index++;
+            }
+        }
+        // 自定义类型存储（如果还有需要的自带类型存储需要继续拓展）
+        else
+        {
+            SaveData(valueType, keyName);
         }
     }
 
@@ -104,6 +134,12 @@ public class PlayerPrefsDataMgr
         return data;
     }
 
+    /// <summary>
+    /// 读取值的方法
+    /// </summary>
+    /// <param name="fieldType"></param>
+    /// <param name="keyName"></param>
+    /// <returns></returns>
     public object LoadValue(Type fieldType, string keyName)
     {
         if (fieldType == typeof(int))
@@ -122,6 +158,31 @@ public class PlayerPrefsDataMgr
         {
             return PlayerPrefs.GetInt(keyName, 0) == 1 ? true : false;
         }
-        return null;
+        else if (typeof(IList).IsAssignableFrom(fieldType))
+        {
+            int count = PlayerPrefs.GetInt(keyName, 0);
+            IList list = Activator.CreateInstance(fieldType) as IList;
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(LoadValue(fieldType.GetGenericArguments()[0], keyName + i));
+            }
+            return list;
+        }
+        else if (typeof(IDictionary).IsAssignableFrom(fieldType))
+        {
+            int count = PlayerPrefs.GetInt(keyName, 0);
+            IDictionary dic = Activator.CreateInstance(fieldType) as IDictionary;
+            Type[] types = fieldType.GetGenericArguments();
+            for (int i = 0; i < count; i++)
+            {
+                dic.Add(LoadValue(types[0], keyName + "_Key_" + i),
+                        LoadValue(types[1], keyName + "_Value_" + i));
+            }
+            return dic;
+        }
+        else
+        {
+            return LoadData(fieldType, keyName);
+        }
     }
 }
--- Assets/Scripts/实践/Test.cs
+++ Assets/Scripts/实践/Test.cs
@@ -8,6 +8,11 @@ public class PlayerInfo
     public string name = "Geon";
     public int age = 24;
     public bool sex = true;
+    public List<int> list = new List<int> { 0, 1, 2, 3 };
+    public Dictionary<int,int> dic = new Dictionary<int, int> {
+        { 0, 1 },
+        { 1, 2 }
+    };
 }
 
 public class Test : MonoBehaviour
@@ -15,6 +20,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // PlayerPrefs.DeleteAll();
         PlayerInfo p = new PlayerInfo();
         PlayerPrefsDataMgr.Instance.SaveData(p, "Player1");
         PlayerInfo pInfo = PlayerPrefsDataMgr.Instance.LoadData(typeof(PlayerInfo), "Player1") as PlayerInfo;
